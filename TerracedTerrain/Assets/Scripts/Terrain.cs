4127373d2360c56e7f674fb051a77e532bf098f3
krzysztofminko <50458873+krzysztofminko@users.noreply.github.com>
2020-12-15 15:46:08 +0100

    Initial commit

---
+++ TerracedTerrain/Assets/Scripts/Terrain.cs
@@ -0,0 +1,255 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerracedTerrain
+{
+	[RequireComponent(typeof(MeshFilter))]
+	public class Terrain : MonoBehaviour
+	{
+		[System.Serializable]
+		public struct Layer
+		{
+			//TODO: ...
+			public Material material;
+			//[MinMaxSlider(0.0f, 1.0f)]
+			//public Vector2 range;
+		}
+		public List<Layer> layers;
+		
+		public IEnumerator GenerateCoroutine(int height, float[,] map)
+		{
+			Vector3Int mapSize = new Vector3Int(map.GetLength(0), height, map.GetLength(1));
+
+			//Generate heightmap mesh
+			List<Vector3> hmpVertices = new List<Vector3>();
+			List<int> hmpTriangles = new List<int>();
+			for (int z = 0; z < mapSize.z; z++)
+				for (int x = 0; x < mapSize.x; x++)
+				{
+					hmpVertices.Add(new Vector3(x * transform.localScale.x, map[x,z] * mapSize.y, z * transform.localScale.z));
+					if (x < mapSize.x - 1 && z < mapSize.z - 1)
+					{
+						hmpTriangles.Add(hmpVertices.Count - 1);
+						hmpTriangles.Add(hmpVertices.Count - 1 + mapSize.x);
+						hmpTriangles.Add(hmpVertices.Count - 1 + 1);
+						hmpTriangles.Add(hmpVertices.Count - 1 + mapSize.x);
+						hmpTriangles.Add(hmpVertices.Count - 1 + mapSize.x + 1);
+						hmpTriangles.Add(hmpVertices.Count - 1 + 1);
+					}
+				}
+			yield return null;
+
+			//Generate Terraces
+			for (int h = 0; h < mapSize.y; h++)
+			{
+				if (layers[h].material)
+				{
+					GameObject go = transform.Find(h.ToString())?.gameObject;
+					if (!go)
+					{
+						go = new GameObject(h.ToString(), typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider));
+						go.transform.parent = transform;
+						go.transform.localPosition = Vector3.zero;
+					}
+					go.GetComponent<MeshRenderer>().sharedMaterial = h < layers.Count ? layers[h].material : null;
+
+					Mesh mesh = new Mesh();
+					mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+					List<Vector3> vertices = new List<Vector3>();
+					List<int> triangles = new List<int>();
+					int v = 0;
+
+					for (int t = 0; t < hmpTriangles.Count / 3; t++)
+					{
+						Vector3 v1 = hmpVertices[hmpTriangles[t * 3]];
+						Vector3 v2 = hmpVertices[hmpTriangles[t * 3 + 1]];
+						Vector3 v3 = hmpVertices[hmpTriangles[t * 3 + 2]];
+
+						float h1 = v1.y;
+						float h2 = v2.y;
+						float h3 = v3.y;
+
+						int hMin = Mathf.FloorToInt(Mathf.Min(h1, h2, h3));
+						int hMax = Mathf.FloorToInt(Mathf.Max(h1, h2, h3));
+
+						//for (int h = hMin; h <= hMax; h++)
+						if (h >= hMin && h <= hMax)
+						{
+							int above = 0;
+
+							if (h1 < h)
+							{
+								if (h2 < h)
+								{
+									if (h3 < h)
+									{
+										//all below
+									}
+									else
+									{
+										above = 1;
+									}
+								}
+								else
+								{
+									if (h3 < h)
+									{
+										above = 1;
+										Vector3 oldv1 = v1;
+										Vector3 oldv2 = v2;
+										Vector3 oldv3 = v3;
+										v1 = oldv3;
+										v2 = oldv1;
+										v3 = oldv2;
+									}
+									else
+									{
+										above = 2;
+										Vector3 oldv1 = v1;
+										Vector3 oldv2 = v2;
+										Vector3 oldv3 = v3;
+										v1 = oldv2;
+										v2 = oldv3;
+										v3 = oldv1;
+									}
+								}
+							}
+							else
+							{
+								if (h2 < h)
+								{
+									if (h3 < h)
+									{
+										above = 1;
+										Vector3 oldv1 = v1;
+										Vector3 oldv2 = v2;
+										Vector3 oldv3 = v3;
+										v1 = oldv2;
+										v2 = oldv3;
+										v3 = oldv1;
+									}
+									else
+									{
+										above = 2;
+										Vector3 oldv1 = v1;
+										Vector3 oldv2 = v2;
+										Vector3 oldv3 = v3;
+										v1 = oldv3;
+										v2 = oldv1;
+										v3 = oldv2;
+									}
+								}
+								else
+								{
+									if (h3 < h)
+									{
+										above = 2;
+									}
+									else
+									{
+										above = 3;
+									}
+								}
+							}
+
+							h1 = v1.y;
+							h2 = v2.y;
+							h3 = v3.y;
+
+							float yScale = transform.localScale.y;
+
+							Vector3 v1_c = new Vector3(v1.x, h * yScale, v1.z);
+							Vector3 v2_c = new Vector3(v2.x, h * yScale, v2.z);
+							Vector3 v3_c = new Vector3(v3.x, h * yScale, v3.z);
+
+							Vector3 v1_b = new Vector3(v1.x, (h - 1) * yScale, v1.z);
+							Vector3 v2_b = new Vector3(v2.x, (h - 1) * yScale, v2.z);
+							Vector3 v3_b = new Vector3(v3.x, (h - 1) * yScale, v3.z);
+
+							if (above == 3)
+							{
+								vertices.Add(v1_c);
+								vertices.Add(v2_c);
+								vertices.Add(v3_c);
+								triangles.Add(v);
+								triangles.Add(v + 1);
+								triangles.Add(v + 2);
+								v += 3;
+							}
+							else
+							{
+								float t1 = (h1 - h) / (h1 - h3);
+								Vector3 v1_c_n = Vector3.Lerp(v1_c, v3_c, t1);
+								Vector3 v1_b_n = Vector3.Lerp(v1_b, v3_b, t1);
+
+								float t2 = (h2 - h) / (h2 - h3);
+								Vector3 v2_c_n = Vector3.Lerp(v2_c, v3_c, t2);
+								Vector3 v2_b_n = Vector3.Lerp(v2_b, v3_b, t2);
+
+								if (above == 2)
+								{
+									vertices.Add(v1_c);
+									vertices.Add(v2_c);
+									vertices.Add(v2_c_n);
+									vertices.Add(v1_c_n);
+									triangles.Add(v);
+									triangles.Add(v + 1);
+									triangles.Add(v + 2);
+									triangles.Add(v + 2);
+									triangles.Add(v + 3);
+									triangles.Add(v);
+									v += 4;
+
+									vertices.Add(v1_c_n);
+									vertices.Add(v2_c_n);
+									vertices.Add(v2_b_n);
+									vertices.Add(v1_b_n);
+									triangles.Add(v);
+									triangles.Add(v + 1);
+									triangles.Add(v + 2);
+									triangles.Add(v);
+									triangles.Add(v + 2);
+									triangles.Add(v + 3);
+									v += 4;
+								}
+								else if (above == 1)
+								{
+									vertices.Add(v3_c);
+									vertices.Add(v1_c_n);
+									vertices.Add(v2_c_n);
+									triangles.Add(v);
+									triangles.Add(v + 1);
+									triangles.Add(v + 2);
+									v += 3;
+
+									vertices.Add(v2_c_n);
+									vertices.Add(v1_c_n);
+									vertices.Add(v1_b_n);
+									vertices.Add(v2_b_n);
+									triangles.Add(v);
+									triangles.Add(v + 1);
+									triangles.Add(v + 3);
+									triangles.Add(v + 1);
+									triangles.Add(v + 2);
+									triangles.Add(v + 3);
+									v += 4;
+								}
+							}
+						}
+					}
+					mesh.vertices = vertices.ToArray();
+					mesh.triangles = triangles.ToArray();
+					mesh.RecalculateNormals();
+					go.GetComponent<MeshFilter>().mesh = mesh;
+					go.GetComponent<MeshCollider>().sharedMesh = mesh;
+					//yield return null;
+				}
+			}
+			for (int i = transform.childCount - 1; i >= mapSize.y; i--)
+				DestroyImmediate(transform.GetChild(i).gameObject);
+		}
+	}
+}
---
+++ TerracedTerrain/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,214 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+	[System.Serializable]
+	public class PerlinLayer
+	{
+		public bool active;
+		[Range(0.00001f, 1.0f)]
+		public float scale;
+		public Vector2 offset;
+		[Range(0.0f, 1.0f)]
+		public float weight;
+		[Range(-1.0f, 1.0f)]
+		public float contrast;
+	}
+
+	public bool generateInEditMode;
+	public Vector3Int mapSize = new Vector3Int(100, 10, 100);
+	[Range(-1.0f, 1.0f)]
+	public float contrast;
+	[SerializeField]
+	private Texture2D loadTexture;
+	[PreviewField(200, ObjectFieldAlignment.Center)][ShowInInspector][ReadOnly]
+	private Texture2D mapTexture;
+	public AnimationCurve fallOff;
+	public PerlinLayer fallOffNoise;
+	public float scale = 1;
+	public List<PerlinLayer> cliffs = new List<PerlinLayer>();
+	public Transform treePrefab;
+	public List<PerlinLayer> trees = new List<PerlinLayer>();
+
+	private float[,] map;
+
+
+	private void OnValidate()
+	{
+		mapSize = Vector3Int.Max(Vector3Int.one * 1, mapSize);
+		if (loadTexture)
+			mapTexture = loadTexture;
+		if (gameObject.activeInHierarchy)
+		{
+			if (generateInEditMode)
+				StartCoroutine(GenerateCoroutine(mapSize));
+			else
+				DestroyChildren();
+		}
+	}
+
+	private void Start()
+	{
+		StartCoroutine(GenerateCoroutine(mapSize));
+	}	
+
+
+	public IEnumerator GenerateCoroutine(Vector3Int mapSize)
+	{
+		float[,] map;
+		float groundHeight = 1.5f * (1.0f / mapSize.y);
+
+		//Generate heightmap
+		if (!loadTexture)
+		{
+			mapTexture = new Texture2D(mapSize.x, mapSize.z);
+			map = new float[mapSize.x, mapSize.z];
+
+			//Cliffs
+			for (int z = 0; z < mapSize.z; z++)
+				for (int x = 0; x < mapSize.x; x++)
+				{
+					//Land height
+					float h = 0;
+					float weightSum = Mathf.Max(1.0f, cliffs.FindAll(l => l.active).Sum(l => l.weight));
+					for (int i = 0; i < cliffs.Count; i++)
+						if (cliffs[i].active)
+						{
+							float p = Mathf.PerlinNoise(x * cliffs[i].scale * scale + cliffs[i].offset.x, z * cliffs[i].scale * scale + cliffs[i].offset.y);
+							float f = (cliffs[i].contrast + 1.0f) / (1.0f - cliffs[i].contrast);
+							p = Mathf.Clamp((f * (p - 0.5f) + 0.5f) * (cliffs[i].weight / weightSum), 0.0f, 0.99f);
+							h += p;
+						}
+					float factor = (contrast + 1.0f) / (1.0f - contrast);
+					h = Mathf.Clamp(factor * (h - 0.5f) + 0.5f, 0.0f, 0.99f);
+					//h *= fallOff.Evaluate((new Vector2(mapSize.x, mapSize.z) * 0.5f - new Vector2(x, z)).magnitude / (new Vector2(mapSize.x, mapSize.z) * 0.5f).magnitude);
+					//h *= fallOff.Evaluate(Distance.Manhattan2D(new Vector2(mapSize.x, mapSize.z) * 0.5f, new Vector2(x, z), Distance.Axis.z) / Distance.Manhattan2D(Vector2.zero, new Vector2(mapSize.x, mapSize.z) * 0.5f, Distance.Axis.z));
+					float horizontal = (Mathf.Abs(mapSize.x * 0.5f - x) / (mapSize.x * 0.5f)) * (1.0f - fallOffNoise.weight) + Mathf.PerlinNoise(z * fallOffNoise.scale + fallOffNoise.offset.y, 0.0f) * fallOffNoise.weight;
+					float vertical = (Mathf.Abs(mapSize.z * 0.5f - z) / (mapSize.z * 0.5f)) * (1.0f - fallOffNoise.weight) + Mathf.PerlinNoise(x * fallOffNoise.scale + fallOffNoise.offset.x, 0.0f) * fallOffNoise.weight;
+					float distance = horizontal > vertical ? horizontal : vertical;
+					h *= fallOff.Evaluate(distance);// * (fallOffNoise.active? Mathf.PerlinNoise(horizontal > vertical ? (z / mapSize.z) : (x / mapSize.x), 0.0f) : 1.0f));
+					h *= 1.0f - groundHeight;
+					h += groundHeight;
+					map[x, z] = Mathf.Clamp(h,0.0f,0.99f);
+					mapTexture.SetPixel(x, z, new Color(h, h, h));
+					/*
+					if(h == groundHeight)
+					{
+						float t = 0;
+						for (int i = 0; i < trees.Count; i++)
+							if (trees[i].active)
+							{
+								float p = Mathf.PerlinNoise(x * trees[i].scale + trees[i].offset.x, z * trees[i].scale + trees[i].offset.y);
+								float f = (trees[i].contrast + 1.0f) / (1.0f - trees[i].contrast);
+								p = Mathf.Clamp((f * (p - 0.5f) + 0.5f) * (trees[i].weight / weightSum), 0.0f, 0.99f);
+								t += p;
+							}
+						if(t < 0.5f)
+						{
+							t = 0;
+						}
+						else
+						{
+							Instantiate(treePrefab, transform.position + new Vector3(x * transform.localScale.x, h* transform.localScale.y, z * transform.localScale.z), Quaternion.AngleAxis(Random.Range(0.0f,360.0f), Vector3.up));
+						}
+					}*/
+				}
+
+			//Rivers
+			/*Vector2Int start = new Vector2Int(20, 20);
+			Vector2 mainDir = Vector2.one;
+			float sectionLength = 5;
+			for (int i = 0; i < 5; i++)
+			{
+				Vector2 d = (mainDir + new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f))).normalized * sectionLength;
+				DrawLine(map, start, start + new Vector2Int((int)d.x, (int)d.y), (1.0f / mapSize.y) * 0.5f, 1, 10);
+				start += new Vector2Int((int)d.x, (int)d.y);
+			}*/
+			/*Vector2 point = new Vector2(20, 20);
+			Vector2 dir = new Vector2(0.5f, 0.5f);
+			for (int i = 0; i < 50; i++)
+			{
+				DrawPoint(map, new Vector2Int((int)point.x, (int)point.y), groundHeight * 0.5f, groundHeight, 5);
+				point += dir;
+			}*/
+
+			//Lakes
+			
+
+			mapTexture.Apply();
+		}
+		else
+		{
+			mapSize = new Vector3Int(loadTexture.width, mapSize.y, loadTexture.height);
+			map = new float[mapSize.x, mapSize.z];
+			for (int z = 0; z < mapSize.z; z++)
+				for (int x = 0; x < mapSize.x; x++)
+					map[x, z] = loadTexture.GetPixel(x, z).r;
+		}
+		this.map = map;
+		this.mapSize = mapSize;
+
+		//Generate meshes
+		yield return StartCoroutine(GetComponent<TerracedTerrain.Terrain>().GenerateCoroutine(mapSize.y, map));
+	}
+
+	private void DestroyChildren()
+	{
+		for (int i = transform.childCount - 1; i >= 0; i--)
+			DestroyImmediate(transform.GetChild(i).gameObject);
+	}
+
+	private void DrawLine(float[,] map, Vector2Int p0, Vector2Int p1, float value, float falloffValue = 0, int radius = 1)
+	{
+		int dx = p1.x - p0.x;
+		int dy = p1.y - p0.y;
+		var sign_x = dx > 0 ? 1 : -1;
+		var sign_y = dy > 0 ? 1 : -1;
+		dx = Mathf.Abs(dx);
+		dy = Mathf.Abs(dy);
+
+		Vector2Int p = new Vector2Int(p0.x, p0.y);
+		DrawPoint(map, p, value, falloffValue, radius);
+		float ix = 0;
+		float iy = 0;
+		while (ix < dx || iy < dy)
+		{
+			if ((0.5 + ix) / dx < (0.5 + iy) / dy)
+			{
+				p.x += sign_x;
+				ix++;
+			}
+			else
+			{
+				p.y += sign_y;
+				iy++;
+			}
+			DrawPoint(map, p, value, falloffValue, radius);
+		}
+	}
+
+	private void DrawPoint(float [,] map, Vector2Int p, float value, float falloffValue = 0, int radius = 5)
+	{
+		if (radius < 0)
+			Debug.LogError("radius must be positive number.", this);
+
+		for (int x = -radius; x <= radius; x++)
+			for (int y = -radius; y <= radius; y++)
+			{
+				float d = (new Vector2Int(x, y) - Vector2Int.zero).magnitude;
+				if (d <= radius)
+				{
+					float v = falloffValue - (falloffValue - value) * (1.0f - d / radius);
+					if (v < map[p.x + x, p.y + y])
+					{
+						map[p.x + x, p.y + y] = v;
+						mapTexture.SetPixel(p.x + x, p.y + y, new Color(v, v, v));
+					}
+				}
+			}
+	}
+}
